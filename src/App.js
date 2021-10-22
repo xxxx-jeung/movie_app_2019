5fd4f747f38924bdcd2e9f1fc2cbd078d68ac808
xxxx-jeung <bioman3238@gmail.com>
2021-10-22 13:57:58 +0900

    JSX, HTML+Javascript

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({fav}){
+  return <h1>나는 {fav} 사랑한다.</h1>
+}
 
 function App() {
   return (
     <div>
-      <section>
-        Table
-      </section>
-      <Potato />
+      <section>Table</section>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="삼겹살"/>
+      <Food fav="쭈꾸미"/>
     </div>
   );
 }
