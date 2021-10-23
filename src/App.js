97d9601e4fa41c3c2c6e17646d128d0f46756c5d
xxxx-jeung <bioman3238@gmail.com>
2021-10-23 17:36:05 +0900

    ComponentMount Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component{
+  constructor(props) {
+    super(props)
+    console.log("hello");
+  };
+
   state = {
     count: 0
   };
@@ -13,12 +18,22 @@ class App extends React.Component{
     this.setState(current => ({ count: current.count-1 }));
   };
 
+  componentDidMount(){
+    console.log("component rendered")
+  }
+
+  componentDidUpdate(){
+    console.log("i just update");
+  }
   render(){
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    console.log("rendering");
+    return ( 
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
 }
 
