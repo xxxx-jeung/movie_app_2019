47252c68e69db42af65215c69e95f0567a5768be
xxxx-jeung <bioman3238@gmail.com>
2021-11-04 16:57:37 +0900

    작업 완료

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import axios from "axios";
+import Movies from "./Movie";
 
-class App extends React.Component{
-  constructor(props) {
-    super(props)
-    console.log("hello");
-  };
-
+class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState(current => ({ count: current.count+1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count-1 }));
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+
+    //console.log(movies.data.data.movies);
+    // console.log(movies);
+
+    this.setState({ movies, isLoading: false });
   };
 
-  componentDidMount(){
-    console.log("component rendered")
+  componentDidMount() {
+    this.getMovies();
   }
 
-  componentDidUpdate(){
-    console.log("i just update");
-  }
-  render(){
-    console.log("rendering");
-    return ( 
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              //console.log(movie.title)
+              <Movies
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
